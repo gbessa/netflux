74bfea3dc34cbac2b69add20a98cd4a37b34014a
Gustavo Bessa <gbvirtual@gmail.com>
2018-11-30 16:52:15 -0200

    Initial commit

---
+++ src/main/java/br/com/hoptech/netflux/NetfluxApplication.java
@@ -0,0 +1,12 @@
+package br.com.hoptech.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfluxApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/hoptech/netflux/domain/Movie.java
@@ -0,0 +1,4 @@
+package br.com.hoptech.netflux.domain;
+
+public class Movie {
+}
